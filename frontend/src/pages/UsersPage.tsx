830f07fe0728b181fe1b9217a08a1c7fadfae413
Serhii Samoliuk <samolyk2005@gmail.com>
2026-03-11 21:40:59 +0100

    fix

--- frontend/src/pages/UsersPage.tsx
+++ frontend/src/pages/UsersPage.tsx
@@ -73,7 +73,7 @@ export default function UsersPage() {
               variant="h3"
               className="text-white font-black tracking-tighter uppercase italic leading-none"
             >
-              TESTING Клієнти <span className="text-orange-500">.DB</span>
+              ONE MORE TESTING Клієнти <span className="text-orange-500">.DB</span>
             </Typography>
             <Typography
               variant="body2"
